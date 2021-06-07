96e5250e34f64f5f9366e8b3b0a577f82454209e
KevinCramer <31591077+KevinCramer@users.noreply.github.com>
2021-06-07 09:17:05 +0100

    Add files via upload

---
+++ pandajjk_anime_follow_like_bot.py
@@ -0,0 +1,40 @@
+
+from instapy import InstaPy
+from instapy import smart_run
+
+my_list = []
+with open('junpei_anime_current_list_to_follow.txt', 'r') as filehandle:
+    for line in filehandle:
+        # remove linebreak which is the last character of the string
+        currentPlace = line[:-1]
+
+        # add item to the list
+        my_list.append(currentPlace)
+
+
+
+
+# login credentials
+
+insta_username = 'pandajjk.anime'
+insta_password = 'Thr33He@rts'
+
+
+# get an InstaPy session!
+# set headless_browser=True to run InstaPy in the background
+session = InstaPy(username=insta_username,
+                  password=insta_password,
+                  headless_browser=True)
+
+with smart_run(session):
+    
+    session.follow_by_list(followlist = my_list[0:30],sleep_delay = 0)
+    session.like_by_users(my_list[0:30], amount=3)
+
+my_list = my_list[30:]
+
+with open('junpei_anime_current_list_to_follow.txt', 'w') as filehandle:
+    for listitem in my_list:
+        filehandle.write('%s\n' % listitem)
+
+
